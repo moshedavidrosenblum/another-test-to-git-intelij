f5813da73ce65c4ee9307961caf8c7b426abde42
moshedavidrosenblum <mosherosenblum1@gmail.com>
2020-03-30 16:59:21 +0300

    ein yeush

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        moshe();
+    }
+    public static void moshe(){
+        System.out.println("hashem shomer alecha");
+    }
+
+}
